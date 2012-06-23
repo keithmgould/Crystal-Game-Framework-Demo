6a63d52a826099318d1f731aa5f5eecd3613c393
Keith Gould <keith@dailymugshot.com>
2012-06-23 17:21:11 -0400

    random latency between 0 - 200 Ms

--- public/js/lib/crystaljs/transport.js
+++ public/js/lib/crystaljs/transport.js
@@ -1,12 +1,12 @@
 define(['underscore', 'crystaljs/api', 'crystaljs/loop'], function (_, CrystaljsApi, CrystaljsLoop) {
 
-  var extraLatency = 20,
+  var extraLatency = 200,
       socket = io.connect("collabfighter.local:3000");
 
   // For testing: Add artificial latency when receiving server messages
   var delayedSocketOn = function (message, fn) {
     socket.on(message, function (data) {
-      console.log("about to delay " + extraLatency + " Ms and its now: " + Date.now());
+      console.log("about to delay " + Math.random() * extraLatency + " Ms and its now: " + Date.now());
       _.delay(fn, extraLatency, data);
       console.log("but continuing on with by biznass");
     });
